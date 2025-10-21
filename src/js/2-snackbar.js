28faeac425cc209af3fa228c447142aac1de2918
unknown <workboar@gmail.com>
2025-10-21 18:35:21 +0300

    hw

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,39 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const delay = Number(form.delay.value);
+  const state = form.state.value;
+
+  createPromise(delay, state)
+    .then((value) => {
+      iziToast.success({
+        title: "Success",
+        message: `✅ Fulfilled promise in ${value}ms`,
+        timeout: value
+      });
+    })
+    .catch((value) => {
+      iziToast.error({
+        title: "Error",
+        message: `❌ Rejected promise in ${value}ms`,
+        timeout: value
+      });
+    });
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === "fulfilled") {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
